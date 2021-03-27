fd1a41b1e1cd32a725cdf544c434a971bf8a080a
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-27 09:28:42 +0000

    made sure I configured the top bar nav

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps /*, AppContext */ } from "next/app";
+import Router from "next/router";
+import nProgress from "nprogress";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+//make sure to see loader at the top
+Router.events.on("routeChangeStart", () => nProgress.start());
+Router.events.on("routeChangeComplete", () => nProgress.done());
 
 export default MyApp;
