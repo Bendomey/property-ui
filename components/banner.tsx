228501205b7d74e3060094e0d0fb2e0474c9e373
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-27 12:32:34 +0000

    added home banner

---
+++ components/banner.tsx
@@ -0,0 +1,45 @@
+import { Fragment } from "react";
+import { SearchBar } from "./search-bar";
+
+export interface Props {}
+
+const Banner: React.FC<Props> = ({}) => {
+  return (
+    <Fragment>
+      <div className={"bannerContainer bg-red-800"}>
+        <div
+          className={
+            "h-full w-full flex justify-center items-center bg-black bg-opacity-50"
+          }
+        >
+          <div>
+            <h1 className={"text-white font-bold text-5xl"}>
+              Find Your <span className={"text-red-200"}>Perfect Property</span>
+            </h1>
+            <div className={"flex justify-center"}>
+              <span className={"text-center text-white text-lg mt-2"}>
+                Search properties that are for sale and for rent
+              </span>
+            </div>
+            <div className={"flex justify-center mt-10"}>
+              <SearchBar />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <style jsx>{`
+        .bannerContainer {
+          height: 70vh;
+          background-image: url("/images/background.jpg");
+          background-repeat: no-repeat;
+          background-position: center; /* Center the image */
+          background-size: cover; /* Resize the background image to cover the entire container */
+          width: 100vw;
+        }
+      `}</style>
+    </Fragment>
+  );
+};
+
+export default Banner;
