74b1b3cdc6de013b8eb87aab00f84fea0ce4390d
Benjamin Domey <domeybenjamin1@gmail.com>
2021-03-27 12:34:16 +0000

    configured nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
 import "../styles/globals.css";
+//for top bar loading
+import "nprogress/nprogress.css";
+
 import type { AppProps /*, AppContext */ } from "next/app";
 import Router from "next/router";
 import nProgress from "nprogress";
+
+nProgress.configure({
+  showSpinner: false,
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
