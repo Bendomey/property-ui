d49c30d37ed004fc613f9029fa523d3d9f873722
Benjamin Domey <domeybenjamin1@gmail.com>
2021-06-06 22:32:14 +0000

    refactor(header): removed mobile dropdown component for now

--- components/header.tsx
+++ components/header.tsx
@@ -107,7 +107,7 @@ const Header: FC<Props> = ({ background }) => {
           </div>
         </div>
 
-        <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
+        {/* <div className="absolute top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden">
           <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 bg-white divide-y-2 divide-gray-50">
             <div className="pt-5 pb-6 px-5">
               <div className="flex items-center justify-between">
@@ -370,6 +370,7 @@ const Header: FC<Props> = ({ background }) => {
             </div>
           </div>
         </div>
+      */}
       </div>
     </Fragment>
   );
